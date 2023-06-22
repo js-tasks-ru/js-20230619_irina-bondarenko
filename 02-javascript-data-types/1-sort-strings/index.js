c3d83fd0d274793bc576cef8b798f4b784a4ee9f
Risha Bondarenko <ibondarenko686@gmail.com>
2023-06-22 21:58:34 +0300

    added 1-module/1-sort-string

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,16 +1,13 @@
-const collator = new Intl.Collator(undefined, { caseFirst: 'upper' });
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
+
+const collator = new Intl.Collator(undefined, { caseFirst: 'upper' });
+
 export function sortStrings(arr, param = 'asc') {
   const res = [...arr].sort((a, b) => collator.compare(a, b));
   return param === 'asc' ? res : res.reverse();
 }
-
-console.log(sortStrings(['b', 'a', 'c'], 'asc'));
-console.log(sortStrings(['b', 'a', 'c'], 'desc'));
-
-console.log(sortStrings(['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'], 'asc'));
